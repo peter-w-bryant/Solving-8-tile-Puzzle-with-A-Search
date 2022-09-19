0f8ec0f0d4ac27871a7d59f13f746a0c1e412e8e
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-09-18 20:45:24 -0500

    Initial commit

--- solve_puzzle.py
+++ solve_puzzle.py
@@ -7,13 +7,11 @@ from itertools import count
 # HELPER FUNCTIONS
 # *************************************************************************************************************************
 
-# Referenced: https://www.adamsmith.haus/python/answers/how-to-convert-a-list-of-integers-into-a-single-integer-in-python
 def state_to_int(state):
     """
     Converts a state to an integer.
     """
     return int("".join(map(str, state)))
-
 
 # *************************************************************************************************************************
 
@@ -21,8 +19,6 @@ def state_to_int(state):
 # Manhattan distance heuristic function
 def get_manhattan_distance(from_state, to_state=[1, 2, 3, 4, 5, 6, 7, 0, 0]):
     """
-    TODO: implement this function. This function will not be tested directly by the grader. 
-
     INPUT: 
         Two states (if second state is omitted then it is assumed that it is the goal state)
 
@@ -41,8 +37,6 @@ def get_manhattan_distance(from_state, to_state=[1, 2, 3, 4, 5, 6, 7, 0, 0]):
 
 def print_succ(state):
     """
-    TODO: This is based on get_succ function below, so should implement that function.
-
     INPUT: 
         A state (list of length 9)
 
@@ -63,7 +57,7 @@ def get_succ(state):
         A state (list of length 9)
 
     RETURNS:
-        A list of all the valid successors in the puzzle (don't forget to sort the result as done below). 
+        A list of all the valid successors in the puzzle. 
     """
     succ_states = []
 
@@ -231,7 +225,7 @@ def solve(state, goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0]):
         An initial state (list of length 9)
 
     WHAT IT SHOULD DO:
-        Prints a path of configurations from initial state to goal state along  h values, number of moves, and max queue number in the format specified in the pdf.
+        Prints a path of configurations from initial state to goal state along  h values, number of moves, and max queue number.
     """
     # Root state
     root_state = state
@@ -316,22 +310,6 @@ def solve(state, goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0]):
                         max_len_pq = len(open)
 
 if __name__ == "__main__":
-    """
-    Feel free to write your own test code here to examine the correctness of your functions. 
-    Note that this part will not be graded.
-    """
+    solve([2, 5, 1, 4, 3, 6, 7, 0, 0])
 
-    # Test: Solve 1
-    solve([3, 4, 6, 0, 0, 1, 7, 2, 5], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-    # Test: Solve 2
-    # solve([6, 0, 0, 3, 5, 1, 7, 2, 4], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-     # Test: Solve 3
-    # solve([0, 4, 7, 1, 3, 0, 6, 2, 5], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-     # Test: Solve 4
-    # solve([5, 2, 3, 0, 6, 4, 7, 1, 0], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-     # Test: Solve 5
-    # solve([1, 7, 0, 6, 3, 2, 0, 4, 5], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-
-    # solve([3, 4, 6, 0, 0, 1, 7, 2, 5], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
-    # solve([2,5,1,4,0,6,7,0,3], goal_state=[1, 2, 3, 4, 5, 6, 7, 0, 0])
 
